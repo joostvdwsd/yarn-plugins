fd9814682e0552d6c08f43ef5ad9a3f0942e4dd0
Joost van der Waal <joost.vanderwaal@aegon.nl>
2022-10-17 20:49:37 +0200

    errorr handling

--- workspaces/plugins/gitversion/src/commands/pack.ts
+++ workspaces/plugins/gitversion/src/commands/pack.ts
@@ -26,58 +26,62 @@ export class GitVersionPackCommand extends BaseCommand {
 
     
     await runStep('Packaging packages', this.context, async (report, configuration) => {
-      if (configuration.versionBranch.branchType === BranchType.UNKNOWN) {
-        report.reportError(MessageName.UNNAMED, 'Running on unknown branch type. Breaking off');
-        return;
-      }   
+      try {
+        if (configuration.versionBranch.branchType === BranchType.UNKNOWN) {
+          report.reportError(MessageName.UNNAMED, 'Running on unknown branch type. Breaking off');
+          return;
+        }   
 
-      const { project } = await Project.find(configuration.yarnConfig, this.context.cwd);
+        const { project } = await Project.find(configuration.yarnConfig, this.context.cwd);
 
-      if (configuration.independentVersioning) {
-        report.reportError(MessageName.UNNAMED, 'IndependentVersioning is not implemented')
-      } else {
-
-        const publicWorkspaces = project.workspaces.filter(this.filterPublicWorkspace);
-        
-        const packFolder = join(project.cwd, this.outputFolder)
-        await mkdir(packFolder, {
-          recursive: true
-        })
-
-        const commands = publicWorkspaces.map((workspace) => {
-          let releaseTagPostfix : string[] = [];
-          if (configuration.versionBranch.branchType === BranchType.FEATURE || configuration.versionBranch.branchType === BranchType.PRERELEASE) {
-            releaseTagPostfix = ['--tag', configuration.versionBranch.name];
-          }
-          report.reportInfo(MessageName.UNNAMED, `Packing ${structUtils.stringifyIdent(workspace.locator)}`)
-
-          return execCapture('yarn', ['pack', '-o', join(packFolder, this.workspacePackageName(workspace))], workspace.cwd);
-        })
-
-        if (this.parallel) {
-          await Promise.all(commands);
+        if (configuration.independentVersioning) {
+          report.reportError(MessageName.UNNAMED, 'IndependentVersioning is not implemented')
         } else {
-          for (let command of commands) {
-            await command;
+
+          const publicWorkspaces = project.workspaces.filter(this.filterPublicWorkspace);
+          
+          const packFolder = join(project.cwd, this.outputFolder)
+          await mkdir(packFolder, {
+            recursive: true
+          })
+
+          const commands = publicWorkspaces.map((workspace) => {
+            let releaseTagPostfix : string[] = [];
+            if (configuration.versionBranch.branchType === BranchType.FEATURE || configuration.versionBranch.branchType === BranchType.PRERELEASE) {
+              releaseTagPostfix = ['--tag', configuration.versionBranch.name];
+            }
+            report.reportInfo(MessageName.UNNAMED, `Packing ${structUtils.stringifyIdent(workspace.locator)}`)
+
+            return execCapture('yarn', ['pack', '-o', join(packFolder, this.workspacePackageName(workspace))], workspace.cwd);
+          })
+
+          if (this.parallel) {
+            await Promise.all(commands);
+          } else {
+            for (let command of commands) {
+              await command;
+            }
           }
+
+          try {
+            const diff = await execCapture('git', ['diff', '--', '*CHANGELOG.md'], project.cwd);
+
+            await writeFile(join(packFolder, 'gitversion.changelog.patch'), diff.result);
+          } catch (error) {
+            report.reportWarning(MessageName.UNNAMED, `Error generating changelog diff: '${error}'`);
+          }
+
+          const configContent = JSON.stringify({
+            versionTag: configuration.versionBranch.name,
+            version: project.topLevelWorkspace.manifest.version,
+            gitTagName: `${configuration.versionTagPrefix}${project.topLevelWorkspace.manifest.version}`,
+            packages: publicWorkspaces.map((workspace) => this.workspacePackageName(workspace))
+          })
+          await writeFile(join(packFolder, 'gitversion.config.json'), configContent, 'utf-8');
+          await writeFile(join(packFolder, 'gitversion.publish.js'), gitversionPublishJavascript, 'utf-8');
         }
-
-        try {
-          const diff = await execCapture('git', ['diff', '--', '*CHANGELOG.md'], project.cwd);
-
-          await writeFile(join(packFolder, 'gitversion.changelog.patch'), diff.result);
-        } catch (error) {
-          report.reportWarning(MessageName.UNNAMED, `Error generating changelog: '${error}'`)
-        }
-
-        const configContent = JSON.stringify({
-          versionTag: configuration.versionBranch.name,
-          version: project.topLevelWorkspace.manifest.version,
-          gitTagName: `${configuration.versionTagPrefix}${project.topLevelWorkspace.manifest.version}`,
-          packages: publicWorkspaces.map((workspace) => this.workspacePackageName(workspace))
-        })
-        await writeFile(join(packFolder, 'gitversion.config.json'), configContent, 'utf-8');
-        await writeFile(join(packFolder, 'gitversion.publish.js'), gitversionPublishJavascript, 'utf-8');
+      } catch (error) {
+        throw error;
       }
     });
   }
